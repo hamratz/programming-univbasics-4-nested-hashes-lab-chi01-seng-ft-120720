4d327ade34fa3da3dfae12732ad85605b5b12447
Marty Warfel <marthawarfel@gmail.com>
2020-11-26 01:55:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,7 +90,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto][0] << [ 
+adding_matz[:yukihiro_matsumoto][0] = [ 
   { :known_for => "Ruby" },
   { :languages => [ "LISP", "C"] }
   ]
