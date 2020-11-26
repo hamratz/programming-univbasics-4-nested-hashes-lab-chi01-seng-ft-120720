d8bcbc01cc568e6ecc2297c6d6f9b617a0fd8856
Marty Warfel <marthawarfel@gmail.com>
2020-11-26 01:23:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,9 +38,11 @@ def alan_kay_is_known_for
     }
     
 end 
-def alan_kay_is_known_for(:alan_kay, :known_for)
   :alan_kay[:known_for]
+
 end
+
+
 
 def dennis_ritchies_language
 	programmer_hash = 
